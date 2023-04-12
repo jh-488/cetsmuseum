9e762e5793dcf1c0359d2622d6af4e70c3564a2e
jh-488 <73128568+jh-488@users.noreply.github.com>
2023-04-12 19:29:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+const upgraded = document.getElementById("yes");
+const notupgraded = document.getElementById("no");
+
+// Get the nft_symbol (CoC+ / CoC) from the metadata of the cet 
+function CetsData(TokenAddress) {
+    // Get all the data about a cet from the solscan API
+    return fetch('https://pro-api.solscan.io/v1.0/nft/token/info/' + TokenAddress, {
+        method: 'Get',
+        headers: {
+            'Accept': 'application/json',
+            'token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOjE2NzkyMzU3NTA1MTcsImVtYWlsIjoiamloYWRyY2FAZ21haWwuY29tIiwiYWN0aW9uIjoidG9rZW4tYXBpIiwiaWF0IjoxNjc5MjM1NzUwfQ.0aCdVk7v7kYOuoFD0Tfos0mFDTrWsxhxV5GmnIWDdvY'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            var nft_symbol = data['data']['0']['nft_symbol'];
+            if(nft_symbol == 'CoC+'){
+                // change the rectangle color to green = Cet upgraded
+                upgraded.classList.add("green");
+                notupgraded.classList.remove("red");
+                return true;
+            }
+            else{
+                // change the rectangle color to red = Cet not upgraded
+                upgraded.classList.remove("green");
+                notupgraded.classList.add("red");
+                return false;
+            }
+        })
+        .catch(error => {
+          console.log('error', error);
+          return false;
+    });
+  }
+
+  
+function GetCets(){
+
+    // Get Cet Address from user input
+    const mintaddress = document.getElementById('cet_address').value;
+
+    // Display Loading icon
+    document.getElementById("loading-icon").style.display = "block";
+
+    // Check if Cet is upgraded
+    CetsData(mintaddress)
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+
+    // Get the versions of the cet from server
+    const E1_version = "https://shdw-drive.genesysgo.net/4ALHMRBZKTApHeCGfMoxPxt1hzg47veMeoarELHkj4TS/" + encodeURIComponent(mintaddress) + "+e1.png";
+    const OG_version = "https://shdw-drive.genesysgo.net/4ALHMRBZKTApHeCGfMoxPxt1hzg47veMeoarELHkj4TS/" + encodeURIComponent(mintaddress) + ".png";
+    const E2_version = "https://shdw-drive.genesysgo.net/4ALHMRBZKTApHeCGfMoxPxt1hzg47veMeoarELHkj4TS/" + encodeURIComponent(mintaddress) + "+e2.png";
+
+    // Create new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+
+    // Open request to the url
+    xhr.open('GET', E1_version);
+
+    // Function to handle the response status
+    xhr.onload = function(){
+        if(xhr.status >= 200 && xhr.status < 300){
+            document.getElementById("error_message").textContent = ""; // clear error message
+
+            document.getElementById('E1').src = E1_version;
+            document.getElementById('OG').src = OG_version;
+            document.getElementById('E2').src = E2_version;
+            // Hide the loading icon
+            document.getElementById("loading-icon").style.display = "none";          
+        }
+        else{
+            // If mintaddress is invalid => bad response status from server => display error message
+            document.getElementById("error_message").textContent = "Invalid Address.. u pessy!!";
+
+            document.getElementById("loading-icon").style.display = "none";
+        }
+    };
+
+    // Send request
+    xhr.send();
+}
+
+document.getElementById("search-btn").addEventListener("click", function() {
+    GetCets();
+  });
+
+
+// Refresh the page on click of the logo/header
+const header = document.getElementById('head');
+const logo = document.getElementById('logo');
+
+header.onclick = () => {
+  location.reload();
+};
+
+logo.onclick = () => {
+  location.reload();
+};
+
+
+// Display the answer after the user hover over the question
+const question = document.getElementById('question');
+const answer = document.getElementById('answer');
+
+question.addEventListener('mouseover', () => {
+    answer.style.display = 'block';
+});
+
+question.addEventListener('mouseout', () => {
+    answer.style.display = 'none';
+});
